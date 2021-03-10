26dd0eaf6dc4e441d44abd7d9c98f49049f84158
Wim Jongman <wim.jongman@remainsoftware.com>
2021-03-10 14:41:39 +0100

    1.9.6 - Website publish
    Change-Id: I7ccc58e9e6b0430b4d4ae63d0a78718049f9432e
    Signed-off-by: Wim Jongman <wim.jongman@remainsoftware.com>

--- download.php
+++ download.php
@@ -41,7 +41,7 @@ Instructions and system requirements for installing WindowBuilder can be found
 	  <td><b>Marketplace</b></td>
 	</tr>
 	<tr>
-	  <td>Latest (1.9.5)</td>
+	  <td>Latest (1.9.6)</td>
 	  <td><a href="http://download.eclipse.org/windowbuilder/latest/">link</a></td>
 	  <td><a href="http://eclipse.org/downloads/download.php?file=/windowbuilder/latest/repository.zip">link</a></td>
 	   <td>
@@ -54,7 +54,7 @@ Instructions and system requirements for installing WindowBuilder can be found
 				<br /><sup>*</sup>Requires Eclipse Marketplace Client</div></a></div>
 	  </td>
 	<tr>
-	  <td>Last good build (1.9.6-pre)</td>
+	  <td>Last good build (1.9.7-pre)</td>
 	  <td><a href="http://download.eclipse.org/windowbuilder/lastgoodbuild/">link</a></td>
 	  <td><a href="http://eclipse.org/downloads/download.php?file=/windowbuilder/lastgoodbuild/repository.zip">link</a></td>
 	  <td>
@@ -66,12 +66,17 @@ Instructions and system requirements for installing WindowBuilder can be found
 				<h3>Drag to Install!</h3>Drag to your running Eclipse<sup>*</sup> workspace.
 				<br /><sup>*</sup>Requires Eclipse Marketplace Client
 			</div></a></div>
-	  </td>
+	  </td> 
 	</tr>
 	<tr>
-	  <td>1.9.5 (Permanent)</td>
-	  <td><a href="http://download.eclipse.org/windowbuilder/1.9.5/">link</a></td>
-	  <td><a href="http://eclipse.org/downloads/download.php?file=/windowbuilder/1.9.5/repository.zip">link</a></td>
+	  <td>1.9.6 (Permanent)</td>
+	  <td><a href="http://download.eclipse.org/windowbuilder/1.9.6/">link</a></td>
+	  <td><a href="http://eclipse.org/downloads/download.php?file=/windowbuilder/1.9.6/repository.zip">link</a></td>
+	</tr>
+	<tr>
+	  <td>1.9.5 (removed)</td>
+	  <td>removed</td>
+	  <td>removed</td>
 	</tr>
 	<tr>
 	  <td>1.9.4 (Permanent)</td>
