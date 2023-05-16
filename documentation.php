9a6c5bc7811dc78deb471aad94574c93703a7d55
Ed Merks <Ed.Merks@gmail.com>
2023-05-16 15:35:48 +0200

    Generate a favicon and use it (#4)

--- documentation.php
+++ documentation.php
@@ -11,6 +11,10 @@
  *******************************************************************************/
 
 require_once ('_projectCommon.php');
+
+// Use our own favicon
+$App->AddExtraHtmlHeader('<link rel="shortcut icon" href="images/favicon.ico"/>');
+
 $pageTitle 		= "WindowBuilder | Documentation";
 $Breadcrumb->removeCrumb($Breadcrumb->getCrumbCount() - 1);
 $Breadcrumb->addCrumb("Documentation", "documentation.php", "_self");
